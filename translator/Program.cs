30417ad60f3c71664066ca7bf87f9216ce3774c6
Grishw <57558364+Grishw@users.noreply.github.com>
2022-10-20 10:11:43 +0300

    first push

---
+++ translator/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+const string MODE_1 = "mealy-to-moore";
+const string MODE_2 = "moore-to-mealy";
+
+
+if (args.Length != 3)
+{
+    Environment.Exit(0);
+};
+Console.WriteLine("Hello, World!");
